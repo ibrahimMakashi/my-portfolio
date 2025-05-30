c448dc674d61f4575096062704b609a9deb10535
ibrahimMakashi <ibrahimmakashi5159@gmail.com>
2025-05-30 19:35:21 +0530

    sixth

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,11 +3,13 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 import swiggy from "../assets/videos/swiggy.mp4";
 import netflix from "../assets/videos/NetflixGPT.mp4";
 import youtube from "../assets/videos/youtube.mp4";
+import { TbPoint } from "react-icons/tb";
 
 const Project = () => {
   const video01 = useRef();
   const video02 = useRef();
   const video03 = useRef();
+
   const handleVideoPlay = (v) => {
     v.current?.play();
   };
@@ -15,9 +17,12 @@ const Project = () => {
   const handleVideoPause = (v) => {
     v.current?.pause();
   };
+
   return (
     <div id="projects" className="my-projects">
       <h1 className="block">My Projects</h1>
+
+      {/* Swiggy Project */}
       <div className="project-card block">
         <div className="project-vid">
           <video
@@ -31,19 +36,30 @@ const Project = () => {
         </div>
         <div className="project-info">
           <h1>
-            Swiggy like <span className="gradient">Food Ordering</span> Web App
+            Swiggy-like <span className="gradient">Food Ordering</span> Web App
           </h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
-            voluptates!
+            <TbPoint /> Used Swiggy's real-time API to fetch restaurant data.
           </p>
-          <a href="https://swiggy-vite-react.netlify.app/" target="blank">
+          <p>
+            <TbPoint /> Performance optimization: infinite scrolling,
+            searching, code-splitting.
+          </p>
+          <p>
+            <TbPoint /> Front-end: ReactJS, HTML, CSS, React Router.
+          </p>
+          <p>
+            <TbPoint /> State management: Redux Toolkit for building Cart.
+          </p>
+          <a href="https://swiggy-vite-react.netlify.app/" target="_blank" rel="noreferrer">
             <button>
               <FaExternalLinkAlt /> Website
             </button>
           </a>
         </div>
       </div>
+
+      {/* NetflixGPT Project */}
       <div className="project-card block">
         <div className="project-vid">
           <video
@@ -57,20 +73,30 @@ const Project = () => {
         </div>
         <div className="project-info">
           <h1>
-            NetflixGPT <span className="gradient">Movies Suggetion</span> Web
-            App
+            NetflixGPT <span className="gradient">Movie Suggestion</span> Web App
           </h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
-            voluptates!
+            <TbPoint /> Used Google's AI model Gemini for movie suggestions.
           </p>
-          <a href="https://net-flix-gpt-ibrahim.netlify.app/" target="blank">
+          <p>
+            <TbPoint /> Used Firebase for user sign-in/sign-up authentication.
+          </p>
+          <p>
+            <TbPoint /> Performance optimization: design inspired by Netflix,
+            responsive across all devices.
+          </p>
+          <p>
+            <TbPoint /> Front-end: ReactJS, HTML, CSS, Redux Toolkit, React Router.
+          </p>
+          <a href="https://net-flix-gpt-ibrahim.netlify.app/" target="_blank" rel="noreferrer">
             <button>
               <FaExternalLinkAlt /> Website
             </button>
           </a>
         </div>
       </div>
+
+      {/* YouTube Clone Project */}
       <div className="project-card block">
         <div className="project-vid">
           <video
@@ -84,13 +110,19 @@ const Project = () => {
         </div>
         <div className="project-info">
           <h1>
-            Youtube <span className="gradient">clone</span> Web App
+            YouTube <span className="gradient">Clone</span> Web App
           </h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
-            voluptates!
+            <TbPoint /> Used YouTube's real-time API to fetch videos.
           </p>
-          <a href="https://youtube-ver-1.netlify.app/" target="blank">
+          <p>
+            <TbPoint /> Performance optimization: search using debouncing,
+            search data caching, suggestions like YouTube, dynamic pages.
+          </p>
+          <p>
+            <TbPoint /> Front-end: ReactJS, HTML, CSS, Redux Toolkit, React Router.
+          </p>
+          <a href="https://youtube-ver-1.netlify.app/" target="_blank" rel="noreferrer">
             <button>
               <FaExternalLinkAlt /> Website
             </button>
